d772a96a767f1ef42203f7a627cc26e2724e2426
NEXUS\Roman <zaro.itstep@gmail.com>
2017-04-03 20:58:04 +0300

    addCash

---
+++ src/main/java/org/itstep/myWebApp/model/CashManager.java
@@ -0,0 +1,27 @@
+package org.itstep.myWebApp.model;
+
+/**
+ * Created by Roman on 03.04.2017.
+ */
+public class CashManager {
+//    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+//            .withCache("preConfigured",
+//                    CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+//                            ResourcePoolsBuilder.heap(100))
+//                            .build())
+//            .build(true);
+//
+//    Cache<Long, String> preConfigured
+//            = cacheManager.getCache("preConfigured", Long.class, String.class);
+//
+//    Cache<Long, String> myCache = cacheManager.createCache("myCache",
+//            CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+//                    ResourcePoolsBuilder.heap(100)).build());
+//
+//      myCache.put(1L, "da one!");
+//    String value = myCache.get(1L);
+//
+//      cacheManager.close();
+
+
+}
